cd385bd077c22ebc469d3c1428b7e0bfe827ad70
Shadrin A <explay97@gmail.com>
2022-03-27 16:54:28 +0300

    create camera, receipt pages

--- js/components/auth.js
+++ js/components/auth.js
@@ -4,7 +4,10 @@ const auth = (containerEl) => {
   const correctCode = (code) => {
     console.log(code);
     setTimeout(() => {
-      window.location.href = "/pages/home.html";
+      window.location.href = window.location.href.replace(
+        "/authentification",
+        "/home"
+      );
     }, 500);
   };
   const checkCode = (phone) => {
@@ -168,4 +171,22 @@ const auth = (containerEl) => {
   };
   login();
 };
+
+const containerEl = document.querySelector(".auth__content");
+const loginEl = document.querySelector(".login");
+const registartionEl = document.querySelector(".registration");
+const navBack = () => {
+  const resetForm = () => {
+    containerEl.querySelectorAll("form").forEach((form) => form.reset());
+  };
+  resetForm();
+  containerEl.className = "auth__content";
+  loginEl.classList.remove("is-hidden");
+  registartionEl.classList.add("is-hidden");
+};
+//  btn Back
+document
+  .querySelectorAll(".auth__back-button")
+  .forEach((btn) => btn.addEventListener("click", navBack));
+auth(containerEl);
 export default auth;
